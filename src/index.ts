bb93b238067ed6a1707d4a12a31d14b0d52316a5
shpilov.a <live-pj@mail.ru>
2021-06-01 20:24:29 +0300

    making boiler

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const app = new PIXI.Application({ sharedTicker: true, sharedLoader: true });
+document.body.appendChild(app.view);
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const isDev = process.env.NODE_ENV === 'development';
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: { main: ['@babel/polyfill', './index.ts'] },
+    mode: process.env.NODE_ENV,
+    output: {
+        filename: `bundle${!isDev ? '.[hash]' : ''}.js`,
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        port: 3000,
+        hot: true
+    },
+    plugins: [
+        new HTMLWebpackPlugin({ template: './index.html' }),
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            PIXI: "pixi.js",
+        }),
+        new CopyWebpackPlugin({
+            patterns:
+                [
+                    {
+                        from: path.resolve(__dirname, "src/assets"),
+                        to: path.resolve(__dirname, "dist/assets"),
+                    }
+                ]
+        }
+        )
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-typescript', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
